7ac2d20fa35f397809baa4351fd4e0abddd24b62
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 16:26:50 +0530

    Adding Loader and Constants file

--- multipleloader/src/main/java/com/anwesome/ui/multipleloader/Constants.java
+++ multipleloader/src/main/java/com/anwesome/ui/multipleloader/Constants.java
@@ -6,5 +6,6 @@ import android.graphics.Color;
  * Created by anweshmishra on 17/04/17.
  */
 public class Constants {
-    public final static int color = Color.parseColor("#AA9E9E9E");
+    public final static int color = Color.parseColor("#99000000");
+    public final static float LOADER_WIDTH = 8;
 }
--- multipleloader/src/main/java/com/anwesome/ui/multipleloader/CustomLoader.java
+++ multipleloader/src/main/java/com/anwesome/ui/multipleloader/CustomLoader.java
@@ -12,6 +12,7 @@ import android.view.View;
 public class CustomLoader extends View {
     private float endDeg = 0;
     private int color = Color.parseColor("#2196F3");
+    private boolean done = false;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public CustomLoader(Context context) {
         super(context);
@@ -23,10 +24,25 @@ public class CustomLoader extends View {
         canvas.drawColor(Constants.color);
         paint.setStyle(Style.STROKE);
         int r = canvas.getWidth()/2;
-        paint.setColor(color);
+        paint.setStrokeWidth(Constants.LOADER_WIDTH);
+        paint.setColor(Color.parseColor("#424242"));
         canvas.drawArc(new RectF(-r,-r,r,r),-90,endDeg,false,paint);
+        paint.setColor(color);
+        canvas.drawArc(new RectF(-r,-r,r,r),-90,360,false,paint);
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
+    }
+    public void update(float factor) {
+        if(factor<=1) {
+            endDeg = 360 * factor;
+            postInvalidate();
+        }
+        else {
+            done = true;
+        }
+    }
+    public boolean isDone() {
+        return done;
     }
 }
