14e3a29493bd7149d036b4c4ffc30b3190e14187
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 15:40:18 +0530

    Adding MultipleLoaderLayout

---
+++ multipleloader/src/main/java/com/anwesome/ui/multipleloader/MultipleLoaderLayout.java
@@ -0,0 +1,19 @@
+package com.anwesome.ui.multipleloader;
+
+import android.content.Context;
+import android.view.*;
+/**
+ * Created by anweshmishra on 17/04/17.
+ */
+public class MultipleLoaderLayout extends ViewGroup{
+    private boolean backgroundDrawn = false;
+    public MultipleLoaderLayout(Context context) {
+        super(context);
+    }
+    public void onLayout(boolean reloaded,int a,int b,int w,int h) {
+
+    }
+    public void onMeasure(int wspec,int hspec) {
+
+    }
+}
