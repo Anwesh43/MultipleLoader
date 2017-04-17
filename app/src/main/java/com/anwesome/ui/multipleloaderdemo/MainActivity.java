0bf70490fb2749fdeb799a7ef3f2f453e06b919b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-17 19:14:17 +0530

    Adding Multiple loader in app

--- app/src/main/java/com/anwesome/ui/multipleloaderdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/multipleloaderdemo/MainActivity.java
@@ -3,11 +3,45 @@ package com.anwesome.ui.multipleloaderdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.multipleloader.MultipleLoaderList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MultipleLoaderList multipleLoaderList = new MultipleLoaderList(this);
+        multipleLoaderList.setSpeed(5);
+        multipleLoaderList.addLoader();
+        multipleLoaderList.addLoader();
+        multipleLoaderList.addLoader();
+        multipleLoaderList.addLoader();
+        multipleLoaderList.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true && !multipleLoaderList.isHidden()) {
+                    try {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                multipleLoaderList.update();
+                            }
+                        });
+
+                        try {
+                            Thread.sleep(200);
+                        }
+                        catch (Exception ex) {
+
+                        }
+                    }
+                    catch (Exception ex) {
+
+                    }
+                }
+            }
+        }).start();
     }
 }
